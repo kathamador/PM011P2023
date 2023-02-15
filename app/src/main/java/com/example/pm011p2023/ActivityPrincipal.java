39087d02f7a32eff8e875efdfa4795fb8ea9cea4
Katherin Amador <katherinamador19@gmail.com>
2023-02-14 20:05:57 -0600

    Cambio realizados el 14/02/2023.

--- app/src/main/java/com/example/pm011p2023/ActivityPrincipal.java
+++ app/src/main/java/com/example/pm011p2023/ActivityPrincipal.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ActivityPrincipal extends AppCompatActivity
 {
     Button btningresar;
     Button btn_lista, btn_combo;
+    ImageButton btnfoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class ActivityPrincipal extends AppCompatActivity
         btningresar = (Button) findViewById(R.id.btn_ingresar);
         btn_lista = (Button) findViewById(R.id.btn_lista);
         btn_combo = (Button) findViewById(R.id.btn_combo);
+        btnfoto = (ImageButton) findViewById(R.id.btnphoto);
 
         btningresar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +44,14 @@ public class ActivityPrincipal extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), ActivityCombo.class);
+                startActivity(intent);
+            }
+        });
+
+        btnfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ActivityPhoto.class);
                 startActivity(intent);
             }
         });
